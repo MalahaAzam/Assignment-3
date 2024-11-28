0be5b35062908f8911360abf78ab2b22ad317679
MalahaAzam <malahah.azam@gmail.com>
2024-11-29 00:50:01 +0500

    first commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+export default function Home() {
+  return (
+    
+    <main className="min-h-screen">
+
+    <div className = "h-96  bg-fixed bg-gradient-to-b from-pink-200 to bg-cyan-200"> 
+    <h1 className="flex justify-center text-6xl pt-20 font-extrabold text-blue-950">Assignment 3</h1>
+    </div>
+    
+    <div className = "h-96  bg-fixed bg-gradient-to-b from-pink-200 to bg-cyan-200"></div>
+
+  
+    </main>
+  );
+}
